bcc3a8d11622ad213823b930a6a12ffd0cf7425a
TimTmi <khoanart05@gmail.com>
2026-04-16 17:51:13 +0700

    feat: add backend search function

---
+++ functions/search.js
@@ -0,0 +1,33 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+
+    if (!q) return new Response("[]");
+
+    const es = await fetch(`${env.ES_URL}/products/_search`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${env.ES_AUTH}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        size: 5,
+        query: {
+          match: {
+            name: {
+              query: q,
+              fuzziness: "AUTO",
+            },
+          },
+        },
+      }),
+    });
+
+    const json = await es.json();
+
+    const results = json.hits.hits.map((h) => h._source.name);
+
+    return Response.json(results);
+  },
+};
